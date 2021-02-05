dcb3f565d11b42071723df5e97f289545be81c73
renzol2 <renzol2@illinois.edu>
2021-02-04 20:37:49 -0600

    Finish part 1

--- mp1/add_int.c
+++ mp1/add_int.c
@@ -4,23 +4,25 @@
 
 // Add the numbers `a` and `b`, returning the result:
 int add_ints(int a, int b) {
-  return 0;
+  return a + b;
 }
 
 
 // Add the contents of the pointer `a` with int `b`, returning the result:
 int add_intMixed(int *a, int b) {
-  return 0;
+  return *a + b;
 }
 
 
 // Add the contents of the pointers `a` and `b` together, returning the result:
 int add_intPtrs(int *a, int *b) {
-  return 0;
+  return *a + *b;
 }
 
 
 // Add the numbers `a` and `b`, returning the result as a pointer in new memory heap memory that you allocate:
 int *add_ints_alloc(int a, int b) {
-  return NULL;
+  int *sum = malloc( sizeof(int) );
+  *sum = a + b;
+  return sum;
 }
