d4b8c1b275863e4d749dd81878d9ad4315fa5568
nozymko <nozymko2@illinois.edu>
2021-02-15 10:21:04 -0600

    Assignment Release

---
+++ mp3/testers/tester3.c
@@ -0,0 +1,30 @@
+/**
+ * Malloc
+ * CS 240 - Fall 2020
+ */
+#include "tester-utils.h"
+
+#define TOTAL_ALLOCS 6000
+#define ALLOC_SIZE 1024 * 1024
+
+int main() {
+    (void) malloc(1);
+
+    int i;
+    void *ptr = NULL;
+
+    for (i = 0; i < TOTAL_ALLOCS; i++) {
+        ptr = malloc(ALLOC_SIZE);
+        if (ptr == NULL) {
+            fprintf(stderr, "Memory failed to allocate!\n");
+            return 1;
+        }
+
+        memset(ptr, 0xab, ALLOC_SIZE);
+
+        free(ptr);
+    }
+
+    fprintf(stderr, "Memory was allocated and freed!\n");
+    return 0;
+}
