6da9367e830cbecb2c85378bc17c94348045cf71
renzol2 <renzol2@illinois.edu>
2021-03-04 16:55:50 -0600

    Finish task 1

--- mp4/msort.c
+++ mp4/msort.c
@@ -6,6 +6,46 @@
 
 
 int main(int argc, char **argv) {
+  if (argc != 3) {
+    printf("No\n");
+  }
 
+  int numSegments = argv[1];
+  FILE* file = fopen(argv[2], "r");
+  
+  // Check that file exists
+  if (file == NULL) {
+    printf("no file :(\n");
+    return 1;
+  }
+
+  // Initialize array
+  const int INITIAL_LENGTH = 100;
+  int currentLength = INITIAL_LENGTH;
+  int* arr = malloc(sizeof(int) * INITIAL_LENGTH);
+
+  // Loop through file
+  const int REALLOC_MULTIPLIER = 2;
+  const int LINE_LENGTH = 100;
+  char line[LINE_LENGTH];
+  int i = 0;
+  while (fgets(line, LINE_LENGTH, file) != NULL) {
+    // Check if line # in file is greater than allocated space
+    if (i >= currentLength - 1) {
+      // Reallocate more memory to array
+      arr = realloc(arr, currentLength * REALLOC_MULTIPLIER);
+      currentLength *= REALLOC_MULTIPLIER;
+      printf("current size: %d\n", currentLength);
+    }
+
+    int num = atoi(line);
+
+    // Add number to array
+    arr[i] = num;
+    i++;
+  }
+
+  int numElements = i;
+  printf("total # of numbers: %d\n", numElements);
 	return 0;
 }
