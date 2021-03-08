a2524cb0d63461670495ad50958a5057b0c9eb2a
renzol2 <renzol2@illinois.edu>
2021-03-08 13:20:26 -0600

    Sort even segments using threads

--- mp4/msort.c
+++ mp4/msort.c
@@ -4,6 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+// The number of elements to sort in thread function
+int sortLength;
+int* arrayToSort;
+
+int compare(const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+void* sortSegment(void* ptr) {
+  printf("Start sortSegment\n");
+  int startIndex = *((int*)ptr);
+  printf("Thread started with value: %d\n", startIndex);
+  qsort(arrayToSort + startIndex, sortLength, sizeof(int), compare);
+  return NULL;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -12,7 +27,8 @@ int main(int argc, char **argv) {
   }
 
   // Get number of segments
-  int numSegments = argv[1];
+  int numSegments = atoi(argv[1]);
+  printf("Number of segments: %d\n", numSegments);
 
   // Initialize array
   const int INITIAL_LENGTH = 100;
@@ -47,11 +63,43 @@ int main(int argc, char **argv) {
   // Reallocate array so it only uses necessary size
   arr = realloc(arr, sizeof(int) * numElements);
 
-  for (int i = 0; i < numElements; i++) {
-    printf("arr[i]: %d\n", arr[i]);
-  }
-
   printf("total # of numbers: %d\n", numElements);
 
+  // Calculate # of values per segment
+  int valuesPerSegment;
+  if (numElements % numSegments == 0) {
+    valuesPerSegment = numElements / numSegments;
+  } else {
+    valuesPerSegment = (numElements / numSegments) + 1;
+  }
+
+  printf("Values per segment: %d\n", valuesPerSegment);
+
+  // If # of values don't divide evenly, find leftover # of values
+  int leftover = numElements - (numSegments - 1) * valuesPerSegment;
+  int hasLeftover = leftover == valuesPerSegment ? 0 : 1;
+
+  // Launch threads to sort
+  pthread_t threads[numSegments];
+  int startIndices[numSegments];
+  for (int i = 0; i < numSegments; i++) {
+    // TODO: change sort size for last segment if uneven
+    sortLength = valuesPerSegment;
+    arrayToSort = arr;
+    startIndices[i] = i * valuesPerSegment;
+    pthread_create(&threads[i], NULL, sortSegment, (void*)&(startIndices[i]));
+  }
+
+  // Join threads
+  for (int i = 0; i < numSegments; i++) {
+    pthread_join(threads[i], NULL);
+  }
+
+  // Print array
+  for (int i = 0; i < numElements; i++) {
+    printf("arr[%d]: %d\n", i, arr[i]);
+  }
+  
+  free(arr);
 	return 0;
 }
