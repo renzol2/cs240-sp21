78bd3b4f086e998ce02b1a57e20cf2641cff68b9
renzol2 <renzol2@illinois.edu>
2021-03-08 11:16:36 -0600

    Use stdin to read numbers

--- mp4/msort.c
+++ mp4/msort.c
@@ -6,46 +6,52 @@
 
 
 int main(int argc, char **argv) {
-  if (argc != 3) {
-    printf("No\n");
-  }
-
-  int numSegments = argv[1];
-  FILE* file = fopen(argv[2], "r");
-  
-  // Check that file exists
-  if (file == NULL) {
-    printf("no file :(\n");
+  if (argc != 2) {
+    printf("Usage: ./msort [NUM SEGMENTS] <[FILE]\n");
     return 1;
   }
 
+  // Get number of segments
+  int numSegments = argv[1];
+
   // Initialize array
   const int INITIAL_LENGTH = 100;
+  const int REALLOC_MULTIPLIER = 2;
   int currentLength = INITIAL_LENGTH;
   int* arr = malloc(sizeof(int) * INITIAL_LENGTH);
 
-  // Loop through file
-  const int REALLOC_MULTIPLIER = 2;
-  const int LINE_LENGTH = 100;
-  char line[LINE_LENGTH];
-  int i = 0;
-  while (fgets(line, LINE_LENGTH, file) != NULL) {
+  int scanResult = 1;
+  int num;
+  int index;
+
+  // While before end of file
+  while (scanResult != -1) {
     // Check if line # in file is greater than allocated space
-    if (i >= currentLength - 1) {
+    if (index >= currentLength - 1) {
       // Reallocate more memory to array
-      arr = realloc(arr, currentLength * REALLOC_MULTIPLIER);
+      arr = realloc(arr, sizeof(int) * currentLength * REALLOC_MULTIPLIER);
       currentLength *= REALLOC_MULTIPLIER;
       printf("current size: %d\n", currentLength);
     }
 
-    int num = atoi(line);
-
-    // Add number to array
-    arr[i] = num;
-    i++;
+    // Load the number into `num` and put it in array
+    scanResult = scanf("%d", &num);
+    arr[index] = num;
+    index++;
   }
 
-  int numElements = i;
+  // Generated sample.txt files usually have an extra line
+  // FIXME: maybe not the best solution
+  int numElements = index - 1;
+
+  // Reallocate array so it only uses necessary size
+  arr = realloc(arr, sizeof(int) * numElements);
+
+  for (int i = 0; i < numElements; i++) {
+    printf("arr[i]: %d\n", arr[i]);
+  }
+
   printf("total # of numbers: %d\n", numElements);
+
 	return 0;
 }
