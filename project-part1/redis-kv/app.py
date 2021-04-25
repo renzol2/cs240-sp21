8386ca7bcfd9831975d5a6da4cee5f6366f364d9
renzol2 <renzol2@illinois.edu>
2021-04-25 00:01:59 -0500

    Copy paste mp8 into redis app

--- project-part1/redis-kv/app.py
+++ project-part1/redis-kv/app.py
@@ -2,11 +2,34 @@ from flask import Flask, render_template, request, jsonify
 import requests
 app = Flask(__name__)
 
-import redis
-r = redis.Redis(host='localhost', port=6379, db=0)
+# State of the server. Stores a key (string)
+# and value (array storing versions of value)
+# Version 1 of a key is at its value[0]
+state = {}
 
-# TODO:
 # PUT /<key> – Adds a versioned object
+@app.route('/<key>', methods=['PUT'])
+def put_key(key):
+  print('State before PUT ' + str(key))
+  print(state)
+
+  # Get value
+  value = request.data.decode('utf-8')
+  
+  # TODO: Insert value at key, checking version too
+
 # GET /<key> – Retrieves the latest version of a key
+@app.route('/<key>', methods=['GET'])
+def get_key(key):
+  # TODO: Get value
+
 # GET /<key>/<version> – Retrieves a specific version of a key
+@app.route('/<key>/<version>', methods=['GET'])
+def get_key_version(key, version):
+  # TODO: Get value at specific key/version
+  
 # DELETE /<key> – Completely deletes a key
+@app.route('/<key>', methods=['DELETE'])
+def delete_key(key):
+  # TODO: Delete key and data
+  return f'{key} deleted successfully', 200
